a392fb81e8a1a747a1b44bb55773b0d091d5a188
niyas <niyaskn01@gmail.com>
2024-02-04 12:12:46 +0530

    mongodb url changed

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -2,11 +2,12 @@ const mongoose=require('mongoose')
 
 const connect=()=>{
 try{
-  mongoose.connect('mongodb://127.0.0.1:27017/Auto')
+  // mongoose.connect('mongodb://127.0.0.1:27017/Auto')
+  mongoose.connect('mongodb+srv://niyaskn01:seencontraniyas@cluster0.h4irhqj.mongodb.net/Auto?retryWrites=true&w=majority')
   console.log('connected with database');
 }
 catch(err){
-  console.log(err);
+  console.log(err); 
 }
 }
 
